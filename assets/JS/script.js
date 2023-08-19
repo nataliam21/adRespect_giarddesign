f56e06bd797d87be72c78f3c144df05252e839ed
natalia_m <n.meisnerowska@gmail.com>
2023-08-19 23:32:33 +0200

    adding the "Projects" section, adding the Macy.Js library, creating js code for the implementation of the Macy.js library, adding a photo gallery for the "Projects" section, Adding a button in the "About Us" section,

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -9,3 +9,17 @@ $(document).ready(function () {
     $("body").removeClass("noscroll");
   });
 });
+
+var macy = Macy({
+  container: ".macy-container", // Klasa kontenera elementów siatki
+  trueOrder: true, // Zachowanie kolejności elementów
+  waitForImages: false, // Czekanie na załadowanie obrazów
+  margin: 43, // Margines między elementami
+  columns: 3, // Ilość kolumn w siatce
+  breakAt: {
+    1200: 3,
+    940: 3,
+    520: 2,
+    400: 2,
+  },
+});
